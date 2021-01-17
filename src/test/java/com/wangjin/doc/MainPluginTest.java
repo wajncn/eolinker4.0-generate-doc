60c61ee9cc21edeb650b592aec494cb940dbd863
wangjin <1253398319@qq.com>
2021-01-17 11:19:17 +0800

    test: test

--- src/test/java/com/wangjin/doc/MainPluginTest.java
+++ src/test/java/com/wangjin/doc/MainPluginTest.java
@@ -1,10 +1,9 @@
-package com.wangjin.doc.handler.impl;
+package com.wangjin.doc;
 
 import com.wangjin.doc.base.Application;
 import com.wangjin.doc.base.Constant;
 import com.wangjin.doc.base.Project;
 import com.wangjin.doc.domain.DocConfig;
-import com.wangjin.doc.handler.LoginDocHandler;
 import com.wangjin.doc.utils.BaseUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +14,7 @@ import java.util.List;
 
 
 @DisplayName("测试")
-public class RequestInfoParseFilterImplTest {
+public class MainPluginTest {
 
     @Test
     void testFirstTest() throws IOException {
@@ -45,7 +44,7 @@ public class RequestInfoParseFilterImplTest {
 
 
     @Test
-    void testFirstTest2() throws IOException {
+    void yipin() throws IOException {
 
         long l = System.currentTimeMillis();
 
@@ -53,7 +52,7 @@ public class RequestInfoParseFilterImplTest {
         Project.LICENSE_STATUS = true;
         Constant.LICENSE_STATUS = true;
 
-        Application.BASE_PATH = "/Users/wangjin/IdeaProjects/yipin";
+        Application.BASE_PATH = "/Users/wangjin/IdeaProjects/chaintrade";
 
         List<String> strings = new ArrayList<>();
         strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/AdImagesController.java");
@@ -84,7 +83,7 @@ public class RequestInfoParseFilterImplTest {
                 .username("wangjin")
                 .password("123456")
                 .projectId("405")
-                .groupId("2945")
+                .groupId("2953")
                 .controllerPaths(strings)
                 .synchronous(true)
                 .update(true)
@@ -93,25 +92,58 @@ public class RequestInfoParseFilterImplTest {
 
         Project project = new Project();
         project.init(Application.BASE_PATH);
+
+        System.out.println("init time: " + (System.currentTimeMillis() - l) + "");
+
+
         project.generate(docConfig.getControllerPaths());
 
-
-        System.out.println("time: " + (System.currentTimeMillis() - l) + "");
+        System.out.println("generate time: " + (System.currentTimeMillis() - l) + "");
         BaseUtils.openBrowse("https://doc.f.wmeimob.com/index.html#/home/project/inside/api/list?projectID=" + docConfig.getProjectId() + "&groupID=" + docConfig.getGroupId());
     }
 
-
     @Test
-    void test() {
+    void abcam() throws IOException {
+
+        long l = System.currentTimeMillis();
+
         Application.LICENSE_STATUS = true;
-        Constant.LICENSE_STATUS = true;
         Project.LICENSE_STATUS = true;
+        Constant.LICENSE_STATUS = true;
+
+        Application.BASE_PATH = "/Users/wangjin/IdeaProjects/abcam";
+
+        List<String> strings = new ArrayList<>();
+        strings.add("/Users/wangjin/IdeaProjects/abcam/abcam-wechat-service/src/main/java/com/abcam/microservice/controller/ActivitySignupController.java");
+        strings.add("/Users/wangjin/IdeaProjects/abcam/abcam-wechat-service/src/main/java/com/abcam/microservice/controller/BannerController.java");
+        strings.add("/Users/wangjin/IdeaProjects/abcam/abcam-wechat-service/src/main/java/com/abcam/microservice/controller/ClassifyController.java");
+        strings.add("/Users/wangjin/IdeaProjects/abcam/abcam-wechat-service/src/main/java/com/abcam/microservice/controller/ClassifyNavigateController.java");
+        strings.add("/Users/wangjin/IdeaProjects/abcam/abcam-wechat-service/src/main/java/com/abcam/microservice/controller/HomeActivityController.java");
+        strings.add("/Users/wangjin/IdeaProjects/abcam/abcam-wechat-service/src/main/java/com/abcam/microservice/controller/NotifyController.java");
+        strings.add("/Users/wangjin/IdeaProjects/abcam/abcam-wechat-service/src/main/java/com/abcam/microservice/controller/PrizeController.java");
+        strings.add("/Users/wangjin/IdeaProjects/abcam/abcam-wechat-service/src/main/java/com/abcam/microservice/controller/StaticController.java");
+        strings.add("/Users/wangjin/IdeaProjects/abcam/abcam-wechat-service/src/main/java/com/abcam/microservice/controller/TestController.java");
+        strings.add("/Users/wangjin/IdeaProjects/abcam/abcam-wechat-service/src/main/java/com/abcam/microservice/controller/UserController.java");
+
         DocConfig docConfig = DocConfig.builder()
                 .username("wangjin")
                 .password("123456")
-                .projectId("405").synchronous(true).build();
+                .projectId("405")
+                .groupId("2951")
+                .controllerPaths(strings)
+                .synchronous(true)
+                .update(true)
+                .build();
         DocConfig.init(docConfig);
-        LoginDocHandler.login();
-        System.out.println(LoginDocHandler.getGroupList());
+
+        Project project = new Project();
+
+        project.init(Application.BASE_PATH);
+
+        System.out.println("init time: " + (System.currentTimeMillis() - l) + "");
+
+        project.generate(docConfig.getControllerPaths());
+        System.out.println("generate time: " + (System.currentTimeMillis() - l) + "");
+        BaseUtils.openBrowse("https://doc.f.wmeimob.com/index.html#/home/project/inside/api/list?projectID=" + docConfig.getProjectId() + "&groupID=" + docConfig.getGroupId());
     }
 }
