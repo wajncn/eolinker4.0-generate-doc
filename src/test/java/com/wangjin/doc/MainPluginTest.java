36800cf940019bccbb3cc24633818d94f039529c
wangjin <1253398319@qq.com>
2021-08-14 21:29:45 +0800

    fix: junit test

--- src/test/java/com/wangjin/doc/MainPluginTest.java
+++ src/test/java/com/wangjin/doc/MainPluginTest.java
@@ -11,13 +11,11 @@ import com.wangjin.doc.base.InterfaceDoc;
 import com.wangjin.doc.base.Project;
 import com.wangjin.doc.handler.ParseHandler;
 import com.wangjin.doc.handler.impl.JavaParseHandlerImpl;
-import com.wangjin.doc.util.BaseUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -25,80 +23,16 @@ import java.util.stream.Collectors;
 
 @DisplayName("测试")
 public class MainPluginTest {
-    @Test
-    void testFirstTest_win10() throws IOException, InterruptedException {
-        Application.basePath = "C:\\Users\\pc\\IdeaProjects\\guanghui-mini";
-
-        DocConfig docConfig = DocConfig.builder()
-                .username("wangjin")
-                .password("123456")
-                .projectId("408")
-                .groupId("2962")
-                .update(true)
-                .build();
-        DocConfig.init(docConfig);
-
-        Project project = new Project();
-        project.init(Application.basePath);
-        project.generate(Collections.singletonList("C:\\Users\\pc\\IdeaProjects\\guanghui-mini\\guanghui-wechat\\src\\main\\java\\com\\wmeimob\\fastboot\\guanghui\\controller\\employees\\EmployeesController.java"));
-//        BaseUtils.openBrowse(docConfig.getUrl() + "/index.html#/home/project/inside/api/list?projectID=" + docConfig.getProjectId() + "&groupID=" + docConfig.getGroupId());
-
-        Thread.sleep(100000 * 5);
-    }
-
-    @Test
-    void testFirstTest() throws IOException {
-        Application.basePath = "/Users/wangjin/IdeaProjects/yipin";
-
-        DocConfig docConfig = DocConfig.builder()
-                .username("wangjin")
-                .password("123456")
-                .projectId("408")
-                .groupId("2963")
-                .update(true)
-                .build();
-        DocConfig.init(docConfig);
-
-        Project project = new Project();
-        project.init(Application.basePath);
-        project.generate(Collections.singletonList("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/DesignOrderController.java"));
-        BaseUtils.openBrowse(docConfig.getUrl() + "/index.html#/home/project/inside/api/list?projectID=" + docConfig.getProjectId() + "&groupID=" + docConfig.getGroupId());
-
-    }
 
 
     @Test
     void yipin() throws IOException, InterruptedException {
-
         long l = System.currentTimeMillis();
         Application.basePath = "/Users/wangjin/IdeaProjects/yipin";
-
         List<String> strings = new ArrayList<>();
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/AdImagesController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/BankCardController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/BillingRecordController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/CaseController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/CompleteTimeConfigController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/CouponController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/CouponRecordController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/DesignerController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/DesignerReviewController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/DesignOrderController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/ExpertiseConfigController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/GlobalConfigController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/IntegralController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/MerchantController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/MerchantReviewController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/OrderDetailController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/PrintOrderController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/ProtocolConfigController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/SwaggerTestController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/SysController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/TextController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/UserInfoController.java");
-        strings.add("/Users/wangjin/IdeaProjects/yipin/yipin-admin/src/main/java/com/wmeimob/fastboot/yipin/controller/WithdrawRecordController.java");
-
+        strings.add("AdImagesController path");
         DocConfig docConfig = DocConfig.builder()
+                .url("wangjin")
                 .username("wangjin")
                 .password("123456")
                 .projectId("408")
@@ -112,13 +46,11 @@ public class MainPluginTest {
         project.init(Application.basePath);
 
         System.out.println("init time: " + (System.currentTimeMillis() - l) + "");
-
         project.generate(docConfig.getControllerPaths());
-
         System.out.println("generate time: " + (System.currentTimeMillis() - l) + "");
 //        BaseUtils.openBrowse(docConfig.getUrl() + "/index.html#/home/project/inside/api/list?projectID=" + docConfig.getProjectId() + "&groupID=" + docConfig.getGroupId());
         System.out.println("---");
-        Thread.sleep(3000*30);
+        Thread.sleep(3000 * 30);
     }
 
 
@@ -128,7 +60,7 @@ public class MainPluginTest {
 
 
     @Test
-    void aaa(){
+    void aaa() {
 //        ReUtil.findAllGroup0(MAPPING_PATTERN, "@RequestMapping(value = \"adsfsf\",method = RequestMethod.GET)").stream()
         ReUtil.findAllGroup0(MAPPING_PATTERN, "@RequestMapping(\"aaa\")").stream()
                 .map(StrUtil::trim)
@@ -138,7 +70,7 @@ public class MainPluginTest {
                         if (annotationExpr instanceof SingleMemberAnnotationExpr) {
                             SingleMemberAnnotationExpr s1 = (SingleMemberAnnotationExpr) annotationExpr;
                             doc.setRequestMapping(s1.getMemberValue().toString().replace("\"", ""));
-                        }else if(annotationExpr instanceof NormalAnnotationExpr){
+                        } else if (annotationExpr instanceof NormalAnnotationExpr) {
                             NormalAnnotationExpr s1 = (NormalAnnotationExpr) annotationExpr;
 //                            doc.setRequestMapping(s1.getMemberValue().toString().replace("\"", ""));
                             System.out.println(1);
